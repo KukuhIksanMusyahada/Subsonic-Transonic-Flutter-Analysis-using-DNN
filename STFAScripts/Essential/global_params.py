ab2e11391dc59fc0bc26d8427b59728583be84ef
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-10 21:28:44 +0700

    Add directories path in global params

---
+++ STFAScripts/Essential/global_params.py
@@ -0,0 +1,11 @@
+
+
+
+# Directories
+DATA_DIRECTORIES = [
+        "Data_Source",
+        "Data_Source/Models",
+        "Data_Source/Raw",
+        "Data_Source/Results",
+        "Data_Source/Processed",
+    ]
