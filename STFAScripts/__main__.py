363af6d949475ac6672e7a7162d8acfd8c5fff70
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-11 00:19:09 +0700

    add init directories

---
+++ STFAScripts/__main__.py
@@ -0,0 +1,21 @@
+
+from Essential import global_params as gp
+from Essential import path_handler as ph
+from Processing_Data import get_data as gd
+
+def runner():
+    # Init Directories
+    ph.InitDataDirectories()
+    # Get The Data
+    
+    # Process the data
+
+
+    # Train Models 
+
+    # Inference 
+
+
+if __name__=='__main__':
+    runner()
+    
