8638b92203376d37b46caf7302ac03ed1941a481
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-11 22:44:22 +0700

    create training_array function done

--- STFAScripts/Processing_Data/data_processing.py
+++ STFAScripts/Processing_Data/data_processing.py
@@ -8,12 +8,28 @@ from Essential import path_handler as ph
 from Essential import global_params as gp
 from Processing_Data import get_data as gd
 
+
+def create_training_array(path):
+    label_array= list()
+    input_array = list()
+    for file in os.listdir(path):
+        if file.endswith('.csv'):
+            file_path = os.path.join(path, file)
+            df = pd.read_csv(file_path, nrows= gp.NROWS, usecols=[1,2,3,4]).to_numpy()
+            input_array.append([gd.extract_mach_and_vf(file)])
+            label_array.append(df)
+    return np.array(input_array), np.array(label_array)
+
+
+
+
+
 def process_data_classification(file='Flutter_Classification_Data.csv',path= ph.get_flutter_class_data(), train_ratio= 0.9):
     input = pd.read_csv(os.path.join(path, file), usecols=['Mach', 'Vf']).to_numpy()
     label = pd.read_csv(os.path.join(path, file), usecols=['Flutter']).to_numpy()
     X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= True)
     return X_train, X_val, y_train, y_val
-    
+
 
 def process_data_flutter():
     pass
