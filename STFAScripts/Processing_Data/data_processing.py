50b2ed025413849c6131f1a943be2af6189cf511
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-11 15:42:53 +0700

    init data_processing.py

---
+++ STFAScripts/Processing_Data/data_processing.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+import numpy as np
+
+from Essential import path_handler as ph
+from Essential import global_params as gp
+from Processing_Data import get_data as gd
+
+def process_data_classification():
+    pass
+
+
+def process_data_flutter():
+    pass
+
+
+def process_data_non_flutter():
+    pass
+
+
+def process_data_transonic():
+    pass
+
