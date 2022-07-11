c47ca5df3cf10cdc90a8f78a161266787402170c
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-11 23:00:53 +0700

    data processing functions done

--- STFAScripts/Processing_Data/data_processing.py
+++ STFAScripts/Processing_Data/data_processing.py
@@ -20,25 +20,32 @@ def create_training_array(path):
             label_array.append(df)
     return np.array(input_array), np.array(label_array)
 
+# def split_training_array(input, label, train_ratio, shuffle= False,):
+#     X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= shuffle)
+#     return X_train, X_val, y_train, y_val
 
 
 
-
-def process_data_classification(file='Flutter_Classification_Data.csv',path= ph.get_flutter_class_data(), train_ratio= 0.9):
+def process_data_classification(file='Flutter_Classification_Data.csv',path= ph.get_flutter_class_data(), train_ratio= gp.TRAIN_RATIO):
     input = pd.read_csv(os.path.join(path, file), usecols=['Mach', 'Vf']).to_numpy()
     label = pd.read_csv(os.path.join(path, file), usecols=['Flutter']).to_numpy()
     X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= True)
     return X_train, X_val, y_train, y_val
 
 
-def process_data_flutter():
-    pass
+def process_data_flutter(path = ph.get_flutter_data(),train_ratio= gp.TRAIN_RATIO):
+    input, label = create_training_array(path)
+    X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= False)
+    return X_train, X_val, y_train, y_val
+
+def process_data_non_flutter(path= ph.get_non_flutter_data(),train_ratio= gp.TRAIN_RATIO):
+    input, label = create_training_array(path)
+    X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= False)
+    return X_train, X_val, y_train, y_val
 
 
-def process_data_non_flutter():
-    pass
-
-
-def process_data_transonic():
-    pass
+def process_data_transonic(path= ph.get_transonic_data(),train_ratio= gp.TRAIN_RATIO):
+    input, label = create_training_array(path)
+    X_train, X_val, y_train, y_val = train_test_split(input,label, train_size= train_ratio, shuffle= False)
+    return X_train, X_val, y_train, y_val
 
