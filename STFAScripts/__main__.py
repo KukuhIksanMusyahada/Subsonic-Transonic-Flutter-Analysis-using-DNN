dc5cf0bddb06391b3e2a5593c5e521d8e1695aed
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-11 23:07:17 +0700

    add processing data to main.py

--- STFAScripts/__main__.py
+++ STFAScripts/__main__.py
@@ -13,11 +13,11 @@ def runner():
     ## Process flutter classification data
     X_class_train, X_class_val, y_class_train, y_class_val = dp.process_data_classification()
     ## Process flutter  data
-
+    X_flutter_train, X_flutter_val, y_flutter_train, y_flutter_val = dp.process_data_flutter()
     ## Process non flutter  data
-
+    X_non_flutter_train, X_non_flutter_val, y_non_flutter_train, y_non_flutter_val = dp.process_data_non_flutter()
     ## Process transonic  data
-
+    X_transonic_train, X_transonic_val, y_transonic_train, y_transonic_val = dp.process_data_transonic()
     # Train Models 
 
     # Inference 
