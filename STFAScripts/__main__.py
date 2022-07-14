ad666d888e845eb7b68b1b2e29f9e0ded9eedb58
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-14 22:53:35 +0700

    untill processing data is clear

--- STFAScripts/__main__.py
+++ STFAScripts/__main__.py
@@ -8,16 +8,16 @@ def runner():
     # Init Directories
     ph.InitDataDirectories()
     # Get The Data
-    gd.scan()
+    max_row, max_row_transonic = gd.scan()
     # Process the data
     ## Process flutter classification data
     X_class_train, X_class_val, y_class_train, y_class_val = dp.process_data_classification()
     ## Process flutter  data
-    X_flutter_train, X_flutter_val, y_flutter_train, y_flutter_val = dp.process_data_flutter()
+    X_flutter_train, X_flutter_val, y_flutter_train, y_flutter_val = dp.process_data_flutter(max_row)
     ## Process non flutter  data
-    X_non_flutter_train, X_non_flutter_val, y_non_flutter_train, y_non_flutter_val = dp.process_data_non_flutter()
+    X_non_flutter_train, X_non_flutter_val, y_non_flutter_train, y_non_flutter_val = dp.process_data_non_flutter(max_row)
     ## Process transonic  data
-    X_transonic_train, X_transonic_val, y_transonic_train, y_transonic_val = dp.process_data_transonic()
+    X_transonic_train, X_transonic_val, y_transonic_train, y_transonic_val = dp.process_data_transonic(max_row_transonic)
     # Train Models 
 
     # Inference 
