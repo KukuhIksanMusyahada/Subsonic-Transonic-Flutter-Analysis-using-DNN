94da64bbf54a4435794d6d14d23b6d8e5bf6b378
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-15 21:00:44 +0700

    add data directories list

--- STFAScripts/Essential/global_params.py
+++ STFAScripts/Essential/global_params.py
@@ -13,6 +13,11 @@ TRAIN_RATIO = 0.9
 DATA_DIRECTORIES = [
         "Data_Source",
         "Data_Source/Models",
+        "Data_Source/Models/Model_Classification",
+        "Data_Source/Models/Model_Non_Flutter",
+        "Data_Source/Models/Model_Flutter",
+        "Data_Source/Models/Model_Transonic",
+        "Data_Source/Models/Model_Master",
         "Data_Source/Raw",
         "Data_Source/Results",
         "Data_Source/Processed",
