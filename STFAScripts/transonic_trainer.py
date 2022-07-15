e15e20731931b5c1ea1177b0cb39861bdee873a2
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-15 21:01:10 +0700

    add transonic trainer

--- STFAScripts/transonic_trainer.py
+++ STFAScripts/transonic_trainer.py
@@ -1,7 +1,21 @@
-
+import datetime
 import numpy as np
 import tensorflow as tf
 
+from Essential import path_handler as ph
+from Training.models import model_transonic, savemodel
 
-def transonic_trainer():
-    pass
+
+def transonic_trainer(x_train,x_val,y_train,y_val, max_row):
+    now = datetime.datetime.now()
+    time_now = now.strftime('%Y-%m-%d-%H:%M:%S')
+    print(f'TRAINING MODELS TRANSONIC START AT {time_now}')
+    model, history = model_transonic(x_train,x_val,y_train,y_val, max_row)
+    savemodel(model, history,type_case=1 , model_path=ph.get_models_non_flutter())
+    end = datetime.datetime.now()
+    time_end = end.strftime('%Y-%m-%d-%H:%M:%S')
+    print(f'TRAINING MODELS TRANSONIC DONE AT {time_end}')
+    delta_time = end-now
+    print(f'TIME NEEDED TO TRAIN TRANSONIC MODEL IS {delta_time}')
+
+    return model, history, delta_time
