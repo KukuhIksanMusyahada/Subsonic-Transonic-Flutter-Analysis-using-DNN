dc1af8747bf77a9c9e6102db552fedcb726f9eb4
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-15 19:31:53 +0700

    change location trainer-inference location to STFAScript

---
+++ STFAScripts/class_trainer.py
@@ -0,0 +1,9 @@
+
+import numpy as np
+import tensorflow as tf
+
+from Training
+
+
+def classification_trainer(x_train,x_val,y_train,y_val, max_row):
+    
--- class_trainer.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-import tensorflow as tf
-
-
-def classification_trainer():
-    pass
