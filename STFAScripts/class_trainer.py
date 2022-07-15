c842fe5069f685301622d18b7d27aa7c587cf6c5
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-15 20:30:21 +0700

    add trainer function for classification case

--- STFAScripts/class_trainer.py
+++ STFAScripts/class_trainer.py
@@ -1,9 +1,23 @@
-
+import datetime
 import numpy as np
 import tensorflow as tf
 
-from Training
+from Essential import path_handler as ph
+from Training.models import model_classification, savemodel
 
 
-def classification_trainer(x_train,x_val,y_train,y_val, max_row):
+def classification_trainer(x_train,x_val,y_train,y_val):
+    now = datetime.datetime.now()
+    time_now = now.strftime('%Y-%m-%d-%H:%M:%S')
+    print(f'TRAINING CLASSIFICATION FLUTTER START AT {time_now}')
+    model, history = model_classification(x_train,x_val,y_train,y_val)
+    savemodel(model, history,type_case=4 , model_path=ph.get_models_classification())
+    end = datetime.datetime.now()
+    time_end = end.strftime('%Y-%m-%d-%H:%M:%S')
+    print(f'TRAINING CLASSIFICATION DONE AT {time_end}')
+    delta_time = end-now
+    print(f'TIME NEEDED TO TRAIN CLASSIFICATION MODEL IS {delta_time}')
+
+    return model, history
+
     
