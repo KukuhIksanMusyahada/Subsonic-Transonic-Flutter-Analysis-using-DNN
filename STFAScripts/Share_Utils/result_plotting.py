d7baba064ecf375c3f86dbc749d622fb3af5bb27
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-16 20:18:48 +0700

    upload shareutils skeleton

---
+++ STFAScripts/Share_Utils/result_plotting.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def history_plot(history):
+    pass
+
+
+def prediction_to_csv(pred):
+    pass
+
+
+def prediction_plot(pred):
+    pass
+
