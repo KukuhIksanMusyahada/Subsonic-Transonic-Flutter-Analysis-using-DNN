1c711d4bbafdf6651a20f74271f58f03ba69bb8b
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-16 12:31:22 +0700

    add case attribute to global params

--- STFAScripts/Essential/global_params.py
+++ STFAScripts/Essential/global_params.py
@@ -1,12 +1,17 @@
-
+from Essential import path_handler as ph
+from Training.trainer_inferencer import *
 
 # Columns
 COLUMNS1 = ['CD', 'CL', 'plunge(airfoil)', 'pitch(airfoil)']
 COLUMNS2 = ['CD', 'CL', 'plunge_airfoil','pitch_airfoil']
-NROWS = 110
 
-# Training
+
+# Training and Inferencing
 TRAIN_RATIO = 0.9
+CASE = ["ModelFlutterClassification", "ModelFlutterPrediction",
+            "ModelNonFlutterPrediction", "ModelTransonicPrediction",
+            "MasterModel"]
+
 
 
 # Directories
@@ -17,7 +22,7 @@ DATA_DIRECTORIES = [
         "Data_Source/Models/Model_Non_Flutter",
         "Data_Source/Models/Model_Flutter",
         "Data_Source/Models/Model_Transonic",
-        "Data_Source/Models/Model_Master",
+        # "Data_Source/Models/Model_Master",
         "Data_Source/Raw",
         "Data_Source/Results",
         "Data_Source/Processed",
